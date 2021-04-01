19eeced21f41619cf7252e99d72924e170ef65d1
Apratim Mahata <apra487@gmail.com>
2021-04-01 23:07:29 +0530

    change data model

--- controllers/domainController.js
+++ controllers/domainController.js
@@ -2,9 +2,7 @@
 function getDomains(req, res) {
     const domains = ["hash.fyi", "hideaddress.net", "mailsire.com", "secret.fyi"]
     res.status(200).json({
-        data: {
-            domains,
-        },
+        domains
     });
 }
 
